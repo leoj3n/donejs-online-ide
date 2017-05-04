a679925fd3bf5b03f217752a875939a46da03dc7
Joel Kuzmarski <leoj3n@gmail.com>
2017-05-04 17:36:46 -0600

    cmdRunner working with file list

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -8,8 +8,6 @@ import loader from '@loader';
 
 const terminal = new Xterm();
 
-terminal.api.writeln('Hello World!');
-
 export const ViewModel = DefineMap.extend({
   message: {
     value: 'This is the ide-editor component'
@@ -17,10 +15,31 @@ export const ViewModel = DefineMap.extend({
   get filesPromise() {
     return Files.getList({});
   },
+  cmdRunner(theCommand) {
+    return fetch(loader.serviceBaseURL + '/cmd', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ cmd: theCommand })
+    }).then(response => response.text());
+  }
 });
 
 export default Component.extend({
-	tag: 'ide-editor',
-	ViewModel,
-	view
+  tag: 'ide-editor',
+  ViewModel,
+  view,
+  events: {
+    inserted(el) {
+      if (System.isPlatform("window")) {
+        this.viewModel.cmdRunner('ls -la').then(function (response) {
+          terminal.api.write(response.replace(/\r?\n/g, "\r\n"));
+        }).catch(function (err) {
+          console.log('COMMAND RUNNER ERROR', err);
+        });
+      }
+    }
+  }
 });
