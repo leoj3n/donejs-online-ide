df7e56ebb91e022af33435f60e842a676f11cc90
imaustink <austin@austinkurpuis.com>
2017-05-04 16:03:23 -0600

    Added donejs-xterm plugin

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -1,16 +1,21 @@
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
+import { Xterm } from 'donejs-xterm';
 import './editor.less';
 import view from './editor.stache';
 
+const terminal = new Xterm();
+
+terminal.api.writeln('Hello World!');
+
 export const ViewModel = DefineMap.extend({
-  message: {
-    value: 'This is the ide-editor component'
-  }
+	message: {
+		value: 'This is the ide-editor component'
+	}
 });
 
 export default Component.extend({
-  tag: 'ide-editor',
-  ViewModel,
-  view
+	tag: 'ide-editor',
+	ViewModel,
+	view
 });
